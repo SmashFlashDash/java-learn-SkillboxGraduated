0207f450d2cc81891605377dad31c664c7ded5bd
Alexandr Sodlatov <salele4715@mail.ru>
2023-04-04 09:07:32 +0300

    добавлено ограничение на добавление сниппета при превышении maxSnippetLength

--- src/main/java/searchengine/config/SearchConfig.java
+++ src/main/java/searchengine/config/SearchConfig.java
@@ -18,7 +18,7 @@ public class SearchConfig {
 
     @PostConstruct
     private void init() {
-        SnippetParser.setSnippetLength(snippetLength);
-        Snippet.setSnippetLength(snippetLength);
+        SnippetParser.setMaxSnippetLength(snippetLength);
+        Snippet.setMaxSnippetLength(snippetLength);
     }
 }
